7a100fd7e5ecb392fc7274f74d348311baa1c153
Eileen Li <eileenlimur@gmail.com>
2021-06-09 16:42:56 -0400

    Add profile image upload capacity (migration, controller)

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -28,10 +29,20 @@ class ProfilesController extends Controller
             'description' => 'required',
             'url' => 'url',
             'image' => '',
-        ]);
+        ]); 
+        
+        if (request('image')) {
+            $imagePath = request('image')->store('profile', 'public');
+            
+            $image = Image::make(public_path("/storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+        }
 
-        auth()->user()->profile->update($data);
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath ?? '']
+        ));
 
-        return redirect("/profile/{$user->id}");
+        return redirect("/profile/ {$user->id}");
     }
 }
