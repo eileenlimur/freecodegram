694e1863a3a5153665135d090d4419a3d9208dda
Eileen Li <eileenlimur@gmail.com>
2021-06-09 14:24:14 -0400

    Fix profile route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ require __DIR__.'/auth.php';
 
 Auth::routes();
 
-Route::get('/profile ', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
 // Route::get('/p/{post}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
