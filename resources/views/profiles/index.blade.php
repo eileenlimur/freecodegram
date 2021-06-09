c0d68f70bdfbb13066c11111dd3e9d4f2e016eeb
Eileen Li <eileenlimur@gmail.com>
2021-06-09 16:10:39 -0400

    Restrict access to edit profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,6 +11,11 @@
                 <h1>{{ $user->username }}</h1>
                 <a href="/p/create">Add New Post</a>
             </div>
+
+            @can('update', $user->profile)
+                <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
+            
             <div class="d-flex">
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>{{ $user->posts()->count() }}</strong> posts</div>
