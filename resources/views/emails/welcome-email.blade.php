e3f55ca3ce4fd9cd909caa1e78f876f5dd441430
Eileen Li <eileenlimur@gmail.com>
2021-06-10 11:26:52 -0400

    Edit welcome email

--- resources/views/emails/welcome-email.blade.php
+++ resources/views/emails/welcome-email.blade.php
@@ -1,12 +1,8 @@
 @component('mail::message')
 # Introduction
 
-The body of your message.
+Welcome to FreeCodeGram. This is a community of fellow developers and we love that you have joined us.
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
-
-Thanks,<br>
-{{ config('app.name') }}
+All the best,<br>
+Eileen
 @endcomponent
