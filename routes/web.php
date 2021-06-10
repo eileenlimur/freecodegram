8607dbc8ba915de538e8e19d7a2735c183e9f178
Eileen Li <eileenlimur@gmail.com>
2021-06-09 23:57:02 -0400

    follow post route

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,12 @@ require __DIR__.'/auth.php';
 
 Auth::routes();
 
+Route::post('follow/{user}', [App\Http\Controllers\FollowsController::class, 'store']);
+
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
 Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index']);
-// Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
 Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
