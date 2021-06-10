e0fa3d7a3b13658d7d01be2e2a545c37e2d88810
Eileen Li <eileenlimur@gmail.com>
2021-06-09 22:19:41 -0400

    Cleanup - may break

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ require __DIR__.'/auth.php';
 
 Auth::routes();
 
-Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
+Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
 Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
-// Route::get('/p/{post}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
-Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index']);
+// Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
 Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
