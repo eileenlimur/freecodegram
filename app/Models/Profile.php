d79777ce753ea71b21be8ee41982654d7f6efff2
Eileen Li <eileenlimur@gmail.com>
2021-06-09 22:18:01 -0400

    create profileImage method with default profileImage

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,8 +9,15 @@ class Profile extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/JrOExveQL036X6SJ8jTNMNEzJH51a9ODllL62Y0G.png';
+        return '/storage/' . $imagePath; 
     }
 }
