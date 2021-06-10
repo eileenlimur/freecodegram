e9715a7981ca9460bc1bc027bae816f3f87dde27
Eileen Li <eileenlimur@gmail.com>
2021-06-09 23:02:16 -0400

    Use profileImage() method

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 p-3 pt-5">
-            <img src="https://img1.luminarypodcasts.com/v1/podcast/4476bc58-b31c-40b5-a63b-270ef0b20cd5/default/h_500,w_500/image/s--5r54k72s--/aHR0cHM6Ly9zc2wtc3RhdGljLmxpYnN5bi5jb20vcC9hc3NldHMvMi9mL2YvNy8yZmY3Y2M4YWEzM2ZlNDM4L2ZyZWVjb2RlY2FtcC1zcXVhcmUtbG9nby1sYXJnZS0xNDAwLmpwZw==.jpg" class="rounded-circle" style="max-width: 100%;"/>
+        <div class="col-3 p-5">
+            <img src="{{ $user->profile->profileImage() }} " class="rounded-circle" style="max-width: 100%;"/>
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
